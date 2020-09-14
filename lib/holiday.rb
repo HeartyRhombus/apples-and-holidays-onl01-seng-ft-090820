fef4ddd476f6fba2e4cb1480a67ecd8fb339eba9
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-14 22:37:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,5 +39,9 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-
+  holiday_hash.collect do |season, holiday|
+    holiday.collect do |holiday, supply|
+      holiday if supply.include?("BBQ")
+    end
+  end.flatten.compact
 end
